3091f7b760e40fb54c4627022770d4f4cde319f8
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-02-20 06:14:43 -0300

    Utilização de fonte baixada do site Google Fonts.

--- app/src/main/java/br/com/fiap/basiccomponents/MainActivity.kt
+++ app/src/main/java/br/com/fiap/basiccomponents/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.fiap.basiccomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,8 +12,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import br.com.fiap.basiccomponents.ui.theme.BasicComponentsTheme
+import br.com.fiap.basiccomponents.ui.theme.Righteous
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +41,23 @@ class MainActivity : ComponentActivity() {
 fun BasicComponentsScreen() {
     Column (
         modifier = Modifier.fillMaxWidth()
+            .fillMaxWidth()
+            .background(Color.Black)
     ) {
-        Text(text = "FIAP")
-        Text(text = "Desenvolvendo aplicações Android")
+        Text(
+            text = "FIAP",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(237, 20, 91),
+            fontFamily = FontFamily.Serif
+            )
+        Text(
+            text = "Desenvolvendo aplicações Android",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.White,
+            fontFamily = Righteous // Utilizando a fonte importada do Google Fonts.
+        )
     }
 }
 
--- app/src/main/java/br/com/fiap/basiccomponents/ui/theme/Type.kt
+++ app/src/main/java/br/com/fiap/basiccomponents/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package br.com.fiap.basiccomponents.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.fiap.basiccomponents.R
+
+// Fontes adicionadas provenientes do site Google Fonts: https://fonts.google.com/
+val Righteous = FontFamily(
+    Font(R.font.righteous_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
